65823c4499c340eff1db6eceddcc883316af34a0
akankshaarushi <115232645+akankshaarushi@users.noreply.github.com>
2023-01-19 16:47:02 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,45 @@
-import React from 'react'
-import '../styles/App.css';
-const App = () => {
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Home from './component/Home';
+import Login from './component/Login';
+import Navbar from './component/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Signup from './component/Signup';
+import Checkout from './component/Checkout';
+import Flightdetail from './component/Flightdetail';
+import Flight from './component/Flight';
+import { createContext, useState } from 'react';
+import Booking from './component/Booking';
+
+export const LoginContext = createContext();
+
+
+function App() {
+
+ const [loggedIn, setLoggedIn] = useState(false);
+
+ const[bookingData ,setBookingData] = useState({});
+
 
 
   return (
-    <div id="main">
+    <div className="App">
+<BrowserRouter>
+<LoginContext.Provider value = {{loggedIn, setLoggedIn}}>
+<Navbar/>
+</LoginContext.Provider>
+<Routes>
+ <Route path='/login' element={<LoginContext.Provider value = {{loggedIn, setLoggedIn, bookingData ,setBookingData}}><Login/></LoginContext.Provider>} />
+ <Route path='/flightdetail' element={<LoginContext.Provider value = {{loggedIn, setLoggedIn, bookingData ,setBookingData}}><Flightdetail/></LoginContext.Provider>} />
+  <Route path='/checkout' element={<LoginContext.Provider value = {{loggedIn, setLoggedIn, bookingData ,setBookingData}}><Checkout/></LoginContext.Provider>} /> 
+ <Route path='/signup' element={<LoginContext.Provider value = {{loggedIn, setLoggedIn, bookingData ,setBookingData}}><Signup/></LoginContext.Provider>} /> 
+ <Route path='/' element={<LoginContext.Provider value = {{loggedIn, setLoggedIn, bookingData ,setBookingData}}><Home/></LoginContext.Provider>} />
+ <Route path='/flight/:index' element={<LoginContext.Provider value = {{loggedIn, setLoggedIn, bookingData ,setBookingData}}><Flight/></LoginContext.Provider>} />
+ <Route path='/booking' element={<LoginContext.Provider value = {{loggedIn, setLoggedIn, bookingData ,setBookingData}}><Booking/></LoginContext.Provider>} />
+</Routes>
+</BrowserRouter>
     </div>
-  )
+  );
 }
-
 
 export default App;
